273d42cab1d0f3a33a5ed188c075fafc492184bf
seonb2n <626ksb@gmail.com>
2023-06-27 19:43:41 +0900

    - 픽셀 느낌 나게 구현 중

--- src/components/CardView.tsx
+++ src/components/CardView.tsx
@@ -2,11 +2,25 @@ import Room from "@/dto/Room";
 
 export default function CardView({roomDto}: Room) {
 
-    console.log(roomDto.hashtags);
-
     return (
-        <div className="w-24 h-80">
+        <div className="w-80 h-48 border">
+            <div className="relative">
+                <div className="bg-white absolute w-6 h-4 top-0 left-0 ">
 
+                </div>
+                <div className="bg-white absolute w-6 h-4 top-0 right-0 ">
+
+                </div>
+                <div className="bg-white absolute w-6 h-4 bottom-0 left-0 ">
+
+                </div>
+                <div className="bg-white absolute w-6 h-4 bottom-0 right-0 ">
+
+                </div>
+                <div>
+                    {roomDto.title}
+                </div>
+            </div>
         </div>
     );
 }
