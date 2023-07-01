c21d0061e0745cf4214a9c6726cb86c8d2201506
seonb2n <626ksb@gmail.com>
2023-07-01 11:52:36 +0900

    [FEATURE] - CardView 추가

--- src/components/CardView.tsx
+++ src/components/CardView.tsx
@@ -1,15 +1,46 @@
 import Room from "@/dto/Room";
 import {SwiperSlide} from "swiper/react";
+import "@/components/CardView.css"
+import Image from "next/image";
 
 export default function CardView({roomDto}: Room) {
 
-    console.log(roomDto);
-
     return (
-        <div className="w-80 h-96 border">
-            <div className="flex h-full justify-center items-center">
-                <div className="text-white">
-                    {roomDto.title}
+        <div className="cardview-container">
+            <div className="cardview_vertex1"></div>
+            <div className="cardview_vertex1-1"></div>
+            <div className="cardview_vertex2"></div>
+            <div className="cardview_vertex2-1"></div>
+            <div className="cardview_vertex3"></div>
+            <div className="cardview_vertex3-1"></div>
+            <div className="cardview_vertex4"></div>
+            <div className="cardview_vertex4-1"></div>
+            <div className="cardview-wrapper bg-gray-700">
+                <div className="h-full">
+                    <div className="h-full w-full p-8">
+                        <div className="text-gray-400 text-left text-sm">
+                            {"#" + roomDto.hashtags.join(" #")}
+                        </div>
+                        <div className="text-white text-left text-3xl font-bold mt-3">
+                            {roomDto.title}
+                        </div>
+
+                        <div className="text-white text-left text-md mt-6">
+                            {roomDto.subtitle}
+                        </div>
+
+                        <div className="flex justify-start mt-4">
+                            <button
+                                className="bg-white hover:bg-blue-500 text-black font-semibold hover:text-white py-2 px-4 rounded">
+                                입장하기
+                            </button>
+                        </div>
+
+                        <div className="flex justify-center">
+                            <Image src={roomDto.iconurl} width={200} height={20} alt={"이미지"}/>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
