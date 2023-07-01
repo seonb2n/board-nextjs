3418f734e641c53baf7a708110cbb75c97efe418
seonb2n <626ksb@gmail.com>
2023-07-01 10:30:04 +0900

    [FEATURE] - Swipe Slide 만들기

--- src/components/CardView.tsx
+++ src/components/CardView.tsx
@@ -1,23 +1,14 @@
 import Room from "@/dto/Room";
+import {SwiperSlide} from "swiper/react";
 
 export default function CardView({roomDto}: Room) {
 
+    console.log(roomDto);
+
     return (
-        <div className="w-80 h-48 border">
-            <div className="relative">
-                <div className="bg-white absolute w-6 h-4 top-0 left-0 ">
-
-                </div>
-                <div className="bg-white absolute w-6 h-4 top-0 right-0 ">
-
-                </div>
-                <div className="bg-white absolute w-6 h-4 bottom-0 left-0 ">
-
-                </div>
-                <div className="bg-white absolute w-6 h-4 bottom-0 right-0 ">
-
-                </div>
-                <div>
+        <div className="w-80 h-96 border">
+            <div className="flex h-full justify-center items-center">
+                <div className="text-white">
                     {roomDto.title}
                 </div>
             </div>
--- src/components/CardViewLayout.tsx
+++ src/components/CardViewLayout.tsx
@@ -1,5 +1,10 @@
 import Room from "@/dto/Room";
 import CardView from "@/components/CardView";
+import {Swiper, SwiperSlide} from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import {Navigation} from "swiper";
 
 export default function CardViewLayout() {
 
@@ -21,13 +26,30 @@ export default function CardViewLayout() {
         "https://cdn-icons-png.flaticon.com/512/2909/2909278.png"
     );
 
-    const tmpData = [room1, room2];
+    const room3 = new Room(
+        3,
+        ["작가", "웹소설"],
+        "작가 작업소",
+        "20/100 명",
+        ["전개가 고구마에요", "마감좀 지켜주세요"],
+        "https://cdn-icons-png.flaticon.com/512/2909/2909278.png"
+    );
+
+    const tmpData = [room1, room2, room3];
 
     return (
-        <div>
-            {
-                tmpData.map(room => <CardView key={room.id} roomDto={room} />)
-            }
+        <div className="w-full h-full flex justify-center items-center">
+            <Swiper navigation={true} modules={[Navigation]}>
+                {
+                    tmpData.map(room => {
+                        return (
+                            <SwiperSlide>
+                                <CardView key={room.id} roomDto={room}/>
+                            </SwiperSlide>
+                        )
+                    })
+                }
+            </Swiper>
         </div>
     );
 }
